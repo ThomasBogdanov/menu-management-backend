6e13d9702789bd9de73dc7efe95e5d307ff93e67
ThomasBogdanov <ThomasBogdanov1@gmail.com>
2022-05-28 19:55:51 -0700

    Refactored tests for easier reading and organization

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,22 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
-  it 'is valid with valid attributes' do
-    menu_item = build(:menu_item)
-    expect(menu_item).to be_valid
+  describe 'Associations' do
+    it 'should belong to a Menu' do
+      should belong_to(:menu)
+    end
+
+    it 'should not be valid without a menu' do
+      menu_item = build(:menu_item, menu_id: nil)
+      expect(menu_item).to_not be_valid
+    end
   end
 
-  it 'is not valid with invlid attributes' do
-    menu_item = build(:menu_item, name: nil)
-    expect(menu_item).to_not be_valid
-  end
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      menu_item = build(:menu_item)
+      expect(menu_item).to be_valid
+    end
 
-  it 'is not valid with an empty name string' do
-    menu_item = build(:menu_item, name: '')
-    expect(menu_item).to_not be_valid
-  end
+    it 'is not valid with invlid attributes' do
+      menu_item = build(:menu_item, name: nil)
+      expect(menu_item).to_not be_valid
+    end
 
-  it 'should belong to a Menu' do
-    should belong_to(:menu)
+    it 'is not valid with an empty name string' do
+      menu_item = build(:menu_item, name: '')
+      expect(menu_item).to_not be_valid
+    end
   end
 end
