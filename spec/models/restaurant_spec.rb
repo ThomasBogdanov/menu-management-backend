92b6b60e03346dea6496a3982d0a85a47b454068
ThomasBogdanov <ThomasBogdanov1@gmail.com>
2022-05-29 16:52:53 -0700

    Added red base restaurant model tests for validation and associations

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it 'should have many menus' do
+      should have_many(:menus)
+    end
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      restaurant = build(:restaurant)
+      expect(restaurant).to be_valid
+    end
+
+    it 'is not valid with invalid attributes' do
+      restaurant = build(:restaurant, name: nil)
+      expect(restaurant).to_not be valid
+    end
+
+    it 'is not valid with an empty name string' do
+      restaurant = build(:restaurant, name: '')
+      expect(restaurant).to_not be_valid
+    end
+
+    it 'should not be valid with a closing time that is before the opening time' do
+      restaurant = build(:restaurant, open_time: Time.now + 2, close_time: Time.now - 2)
+      expect(restaurant).to_not be_valid
+    end
+  end
 end
