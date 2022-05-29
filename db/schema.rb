47300a26a8d42a3e22cca679fa07021e9ae82ff4
ThomasBogdanov <ThomasBogdanov1@gmail.com>
2022-05-29 04:00:51 -0700

    Added restaurants have many menus associations in models

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,31 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_528_042_051) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_29_105933) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'menu_items', force: :cascade do |t|
-    t.bigint 'menu_id', null: false
-    t.string 'name'
-    t.decimal 'price'
-    t.text 'description'
-    t.float 'quantity'
-    t.boolean 'is_active'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['menu_id'], name: 'index_menu_items_on_menu_id'
+  create_table "menu_items", force: :cascade do |t|
+    t.bigint "menu_id", null: false
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.float "quantity"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
   end
 
-  create_table 'menus', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.time 'start_time'
-    t.time 'end_time'
-    t.boolean 'is_active'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.time "start_time"
+    t.time "end_time"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
-  add_foreign_key 'menu_items', 'menus'
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address_line1"
+    t.string "address_line2"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "postal_code"
+    t.string "phone_number1"
+    t.string "phone_number2"
+    t.string "fax_number"
+    t.string "email"
+    t.string "website"
+    t.string "price_range"
+    t.time "open_time"
+    t.time "close_time"
+    t.string "category"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "menu_items", "menus"
+  add_foreign_key "menus", "restaurants"
 end
