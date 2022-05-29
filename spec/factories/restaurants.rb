fb454c8df970e47b6292e89d7d68dee247e6a71c
ThomasBogdanov <ThomasBogdanov1@gmail.com>
2022-05-29 03:48:21 -0700

    Added base restaurant model, migration, empty factory+spec

---
+++ spec/factories/restaurants.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :restaurant do
+    name { "MyString" }
+    address_line1 { "MyString" }
+    address_line2 { "MyString" }
+    city { "MyString" }
+    state { "MyString" }
+    country { "MyString" }
+    postal_code { "MyString" }
+    phone_number1 { "MyString" }
+    phone_number2 { "MyString" }
+    fax_number { "MyString" }
+    email { "MyString" }
+    website { "MyString" }
+    price_range { "MyString" }
+    open_time { "2022-05-29 03:46:26" }
+    close_time { "2022-05-29 03:46:26" }
+    category { "MyString" }
+    is_active { false }
+  end
+end
