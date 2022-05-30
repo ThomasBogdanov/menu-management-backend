9728fea7a87629ccd2275f7c9cd2319efaad408d
ThomasBogdanov <ThomasBogdanov1@gmail.com>
2022-05-29 21:35:56 -0700

    Corrected assertion typo

--- spec/models/item_name_spec.rb
+++ spec/models/item_name_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ItemName, type: :model do
   describe 'Associations' do
     it 'should belong to many items' do
-      should has_many(:items)
+      should have_many(:items)
     end
   end
 end
